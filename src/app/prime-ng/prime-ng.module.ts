ba0fd27838ce90c6ba8bbacee486e25b47d16266
HuguitoS2r <h.huichalaogarcia@gmail.com>
2024-09-16 22:33:04 -0300

    first commit

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,17 @@
+import { MenuModule } from 'primeng/menu';
+import { NgModule } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MenuModule,
+    MenubarModule
+  ]
+  
+})
+export class PrimeNgModule { }
